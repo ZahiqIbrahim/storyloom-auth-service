e6e96979f6ee2e12dcceddb618c1dfb6a9b28720
Zahiq Ibrahim <161963558+ZahiqIbrahim@users.noreply.github.com>
2026-06-08 19:59:46 +0530

    Project setup for storyloom auth service

---
+++ src/main/java/com/example/storyloom_auth_service/StoryloomAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.storyloom_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryloomAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoryloomAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/storyloom_auth_service/StoryloomAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storyloom_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryloomAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
